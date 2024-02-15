019477d9406dcd07f6f260010302845bc2fc686a
aanujkhurana <aanujkhurana@gmail.com>
2024-02-15 16:45:25 +1000

    ADD PIN

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import shareVideo from "../assets/share.mp4"
 import logo from "../assets/logo.png"
 
-import { GoogleLogin, googleLogout} from '@react-oauth/google';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 
 import { client } from '../client';
@@ -14,7 +14,7 @@ const Login = () => {
     const decoded = jwtDecode(response.credential);
     console.log(decoded);
     localStorage.setItem('user', JSON.stringify(decoded));
-
+    console.log(decoded);
     const { name, sub, picture } = decoded;
 
     const doc = {
@@ -25,8 +25,8 @@ const Login = () => {
     }
 
     client.createIfNotExists(doc)
-      .then(() => {})
-      navigate('/',{replace: true});
+      .then(() => { })
+    navigate('/', { replace: true });
   }
 
   return (
@@ -42,19 +42,19 @@ const Login = () => {
           className="w-full h-full object-cover"
         />
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 right-0 bottom-0'>
-        <div className='p-5'>
-          <img src={logo} alt="logo" width="130px" />
-        </div>
-        <div className='shadow-2xl'>
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            responseGoogle(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />;
-        </div>
+          <div className='p-5'>
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              onSuccess={credentialResponse => {
+                responseGoogle(credentialResponse);
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+            />;
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -18,11 +18,11 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save, userId } }) => {
     const alreadySaved = !!(save?.filter((item) => item.userId === user._id))?.length;
 
     const savePin = (id) => {
-        if(!alreadySaved){
+        if (!alreadySaved) {
             client
                 .patch(id)
                 .setIfMissing({ save: [] })
-                .insert ('after', 'save[-1]', [
+                .insert('after', 'save[-1]', [
                     {
                         _key: uuidv4(),
                         userId: user.sub,
@@ -50,86 +50,86 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save, userId } }) => {
     return (
         <div className='m-2'>
             <div
-            onMouseEnter={() => setPostHovered(true)}
-            onMouseLeave={() => setPostHovered(false)}
-            onClick={() => navigate(`/pin/${_id}`)}
-            className='relative cursor-zoom-in w-auto hover:shadow-lg rouded-lg overflow-hidden transition-all duration-500 ease-in-out'
+                onMouseEnter={() => setPostHovered(true)}
+                onMouseLeave={() => setPostHovered(false)}
+                onClick={() => navigate(`/pin-detail/${_id}`)}
+                className='relative cursor-zoom-in w-auto hover:shadow-lg rouded-lg overflow-hidden transition-all duration-500 ease-in-out'
             >
-                <img 
-                className='rounded-lg w-full h-auto'
-                src={urlFor(image).width(250).url()} alt="pin" />
+                <img
+                    className='rounded-lg w-full h-auto'
+                    src={urlFor(image).width(250).url()} alt="pin" />
                 {PostHovered && (
                     <div
-                    className='absolute top-0 left-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
-                    style={{ height: '100%' }}
+                        className='absolute top-0 left-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
+                        style={{ height: '100%' }}
                     >
                         <div className='flex items-center justify-between'>
                             <div className='flex gap-2'>
                                 <a href={`${image?.asset?.url}?dl=`}
-                                download
-                                onClick={(e) => e.stopPropagation()}
-                                className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
+                                    download
+                                    onClick={(e) => e.stopPropagation()}
+                                    className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
                                 >
                                     <MdDownloadForOffline />
                                 </a>
                             </div>
                             {alreadySaved ? (
                                 <button
-                                type='button'
-                                className='bg-red-900 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none'
+                                    type='button'
+                                    className='bg-red-900 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none'
                                 >
-                                    { save?.length } Saved
+                                    {save?.length} Saved
                                 </button>
-                            ):(
+                            ) : (
                                 <button
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    savePin(_id);
-                                
-                                }}
-                                type='button'
-                                className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none'
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        savePin(_id);
+
+                                    }}
+                                    type='button'
+                                    className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none'
                                 >
-                                    { save?.length } Save
+                                    {save?.length} Save
                                 </button>
                             )}
                         </div>
                         <div className='flex justify-between items-center gap-2 w-full'>
-                                {destination && (
-                                    <a
+                            {destination && (
+                                <a
                                     href='{destination}'
                                     target='_blank'
                                     rel='noreferrer'
                                     className='bg-white flex items-center g-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'
-                                    >
-                                        <BsFillArrowUpRightCircleFill className='mr-1'/> 
-                                        {destination.slice(11,28)}
-                                    </a>
-                                )}
-                                {userId === user._id && (
-                                    <button
+                                >
+                                    <BsFillArrowUpRightCircleFill className='mr-1' />
+                                    {destination.slice(11, 28)}
+                                </a>
+                            )}
+                            {userId === user._id && (
+                                <button
                                     onClick={(e) => {
                                         e.stopPropagation();
                                         deletePin(_id);
                                     }}
                                     type='button'
                                     className='bg-white text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'
-                                    >
-                                        <AiTwotoneDelete />
-                                    </button>
-                                )}
+                                >
+                                    <AiTwotoneDelete />
+                                </button>
+                            )}
                         </div>
                     </div>
                 )}
             </div>
             <Link
-            to={`/user-profile/${postedBy?._id}`}
-            className='flex gap-2 mt-2 items-center'
+                to={`/user-profile/${postedBy?._id}`}
+                className='flex gap-2 mt-2 items-center'
             >
                 <img
-                src={postedBy?.image}
-                alt='user-pic'
-                className='w-8 h-8 rounded-full object-cover'
+                    src={postedBy?.image}
+                    alt='user-pic'
+                    className='w-8 h-8 rounded-full object-cover'
                 />
                 <p className='font-medium capitalize'>{postedBy?.userName}</p>
             </Link>
