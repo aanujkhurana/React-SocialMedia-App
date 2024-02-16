188728b592b6d846fad502a20e0eee54c7323127
aanujkhurana <aanujkhurana@gmail.com>
2024-02-17 01:01:42 +1000

    Complete🎉

--- frontend/src/components/MasoryLayout.jsx
+++ frontend/src/components/MasoryLayout.jsx
@@ -7,16 +7,14 @@ import Pin from './Pin'
 const MasoryLayout = ({ pins }) => {
     return (
         <ResponsiveMasonry
-            columnsCountBreakPoints={{ 150: 1, 250: 2, 400: 3, 750: 4, 900: 6 }}
+            columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
         >
             <Masonry className="flex animate-slide-fwd">
-                <>
-                    {pins?.map((pin) => (
-                        <div key={pin._id} className="w-max">
-                            <Pin pin={pin} />
-                        </div>
-                    ))}
-                </>
+                {pins?.map((pin) => (
+                    <div key={pin._id} className="w-max">
+                        <Pin pin={pin} />
+                    </div>
+                ))}
             </Masonry>
         </ResponsiveMasonry>
     )
